781337f6bb30230065e66a1cfb7815a08aec586a
Aditya Maurya <aditya.maurya@nielsen.com>
2026-09-12 15:21:08 +0530

    chore: scaffold Spring Boot project (Boot 4.1.1, Java 21, Gradle)

---
+++ src/main/java/com/example/wallet_transfer/WalletTransferApplication.java
@@ -0,0 +1,13 @@
+package com.example.wallet_transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletTransferApplication.class, args);
+	}
+
+}
